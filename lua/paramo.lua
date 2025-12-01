80b5d43bd3ec844f36408f4ca55cf5c16f1b1682
aidancz <aidanczhai@gmail.com>
2025-12-01 11:45:02 +0800

    simplify dot-repeat

--- lua/paramo.lua
+++ lua/paramo.lua
@@ -43,21 +43,78 @@ end
 
 -- # the following is for dot-repeat
 
----@param opts {
----	count: number,
----	direction: "prev"|"next",
----	is: function,
----	hook?: function,
+M.cache_n = {
+-- normal mode (include visual mode)
+	count = 1,
+	direction = "next",
+	is = function(pos) return true end,
+}
+
+M.cache_o = {
+-- operator pending mode
+	count = 1,
+	direction = "next",
+	is = function(pos) return true end,
+}
+
+M.cache_n_apply = function()
+	if vim.v.count ~= 0 then
+		M.cache_n.count = vim.v.count
+	end
+	M.set_cursor(
+		M.cache_n.count,
+		M.cache_n.direction,
+		M.cache_n.is
+	)
+end
+
+M.cache_o_apply = function()
+	if vim.v.count ~= 0 then
+		M.cache_o.count = vim.v.count
+	end
+	M.start_visual_mode()
+	M.set_cursor(
+		M.cache_o.count,
+		M.cache_o.direction,
+		M.cache_o.is
+	)
+end
+
+---@param opts? {
+---	count?: number,
+---	direction?: "prev"|"next",
+---	is?: function,
 ---}
-M.set_cursor_opts = function(opts)
-	M.set_cursor(opts.count, opts.direction, opts.is)
-	if opts.hook ~= nil then
-		opts.hook()
+M.expr = function(opts)
+	if M.is_operator_pending_mode() then
+		M.cache_o = vim.tbl_extend(
+			"force",
+			{
+				count = 1,
+				direction = "next",
+				is = function(pos) return true end,
+			},
+			opts or {}
+		)
+		return [[<cmd>lua require("paramo").cache_o_apply()<cr>]]
+	else
+		M.cache_n = vim.tbl_extend(
+			"force",
+			{
+				count = 1,
+				direction = "next",
+				is = function(pos) return true end,
+			},
+			opts or {}
+		)
+		return [[<cmd>lua require("paramo").cache_n_apply()<cr>]]
 	end
 end
 
-M.cache_opts_operator_pending_mode = nil
-M.cache_opts_operator_pending_mode_not = nil
+M.is_operator_pending_mode = function()
+	local mode = vim.api.nvim_get_mode().mode
+	return string.sub(mode, 1, 2) == "no"
+end
 
 M.start_visual_mode = function()
 	local mode = vim.api.nvim_get_mode().mode
@@ -76,45 +133,6 @@ M.start_visual_mode = function()
 	end)
 
 	vim.cmd("normal! " .. vis_mode)
-end
-
-M.apply_cache_opts = function(is_operator_pending_mode)
-	local cache_opts
-
-	if is_operator_pending_mode then
-		cache_opts = M.cache_opts_operator_pending_mode
-		M.start_visual_mode()
-	else
-		cache_opts = M.cache_opts_operator_pending_mode_not
-	end
-
-	if vim.v.count ~= 0 then
-		cache_opts.count = vim.v.count
-	end
-
-	M.set_cursor_opts(cache_opts)
-end
-
----@param opts {
----	count?: number,
----	direction: "prev"|"next",
----	is: function,
----	hook?: function,
----}
-M.expr = function(opts)
-	opts.count = opts.count or vim.v.count1
-
-	local mode = vim.api.nvim_get_mode().mode
-	local is_operator_pending_mode = string.sub(mode, 1, 2) == "no"
-	if is_operator_pending_mode then
-		M.cache_opts_operator_pending_mode = opts
-		return
-		[[<cmd>lua require("paramo").apply_cache_opts(true)<cr>]]
-	else
-		M.cache_opts_operator_pending_mode_not = opts
-		return
-		[[<cmd>lua require("paramo").apply_cache_opts(false)<cr>]]
-	end
 end
 
 return M
